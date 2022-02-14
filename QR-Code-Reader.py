38b433351775a02d2d33c90bc5e7812190c2e7f9
Jelisha Ruth Bugnon <73575184+jelisharbb@users.noreply.github.com>
2022-02-14 18:18:06 +0800

    Generated a QR Code that contains a text file.

--- QR-Code-Reader.py
+++ QR-Code-Reader.py
@@ -11,3 +11,17 @@
 # 	- Search how to create and write to text file
 # 	- Your source code should be in github before Feb 19
 # 	- Create a demo of your program (1-2 min) and send it directly to my messenger.
+
+
+""" Generate QR Code that contains the personal data stored in a text file. """
+
+# import qrcode library
+import qrcode
+
+# open and read the text file
+with open('Personal-Data.txt') as personalData:
+    data = personalData.read()
+
+# generate QR code
+QRCode = qrcode.make(data)
+QRCode.save('QRCode.jpg')
