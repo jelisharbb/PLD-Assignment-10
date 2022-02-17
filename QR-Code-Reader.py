79a9d1def0526c748d1f3cf4a6159c217e53a8f1
Jelisha Ruth Bugnon <73575184+jelisharbb@users.noreply.github.com>
2022-02-18 00:24:20 +0800

    Added comments.

--- QR-Code-Reader.py
+++ QR-Code-Reader.py
@@ -12,27 +12,32 @@
 # 	- Your source code should be in github before Feb 19
 # 	- Create a demo of your program (1-2 min) and send it directly to my messenger.
 
-""" Reads the QR Code through web camera then displays the data stored in it. """
+""" Reads the QR Code through web camera then displays the data stored in it in a new text file. """
 
+# import all libraries to be used
+# the cv2 library for live webcams; the pyzbar library for decoding QR codes; and the datetime library for time and date stamping
 import cv2; from pyzbar import pyzbar; from datetime import *
 
+# define a function that stores all the codes needed to read the QR code
 def readQR ():
     capture = cv2.VideoCapture(0)
-    while True:
+    while True: # this loop is to prevent the web camera from closing unless it is intended to
         _, image = capture.read()
-        image = decodeQR(cv2.resize(image, None, fx = 1.0, fy = 1.0, interpolation = cv2.INTER_AREA))
-        cv2.imshow("QR Code Scanner", image)
+        image = decodeQR(cv2.resize(image, None, fx = 1.0, fy = 1.0, interpolation = cv2.INTER_AREA)) # the image that will be scanned on the web cam will be resized according to the parameters i set, and will also be decoded
+        cv2.imshow("QR Code Scanner", image) # the live camera will run on the screen
         if cv2.waitKey(1) == ord('q'):
-            break
+            break # the live camera will close if you press 'q'
     capture.release()
-    cv2.destroyAllWindows()
+    cv2.destroyAllWindows() # this code will fully shut down the web camera
 
+# define another function that contains all the codes needed to decode the QR code
 def decodeQR (image):
-    qrCode = pyzbar.decode(image)
+    qrCode = pyzbar.decode(image) # the scanned QR code will be decoded here
     for details in qrCode:
         qrData = details.data.decode('utf-8')
-        with open ("QR-Code-Result.txt", "w") as textfile:
-            textfile.write(qrData)
+        with open ("QR-Code-Result.txt", "w") as textfile: 
+            textfile.write(qrData) # the decoded data will now be opened and written in a new text file
     return image
 
+# call the function to execute the reading of the QR code
 readQR()
